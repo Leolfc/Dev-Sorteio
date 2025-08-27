770e044e663a2146b89dd7d06364354747a8cee3
Leolfc <leonardofeliciano81@gmail.com>
2025-08-27 15:43:17 -0300

    adicionando botão limpar e melhorando o codigo

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const primeiroInput = document.getElementById('primaryNumber')
-const segundoInput = document.getElementById('secondaryNumber')
-const button = document.querySelector('button')
-button.addEventListener('click', (event)=>{
-    event.preventDefault()
-    const gerarNumeroAleatorio =parseInt( Math.floor(Math.random()*primeiroInput.value - segundoInput.value))
-    alert(`O numero gerado foi ${gerarNumeroAleatorio}`)
-    primeiroInput.value = ''
-    segundoInput.value = ''
-})
+const button = document.querySelector("button");
+const buttonClean = document.querySelector("#cleanNumber");
+const container_input = document.querySelector('.container_input')
+button.addEventListener('click',()=> {
+  const min = Math.ceil(document.getElementById("inputMin").value);
+  const max = Math.floor(document.getElementById("inputMax").value);
+const result =  Math.floor(Math.random() * (max - min + 1)) + min;
+const h3 = document.createElement("h3");
+container_input.appendChild(h3);
+h3.innerHTML = result
 
+h3.style.fontSize = "50px";
+h3.classList.add('numeroSorteado')
+h3.style.textAlign = "center";
+
+
+    
+})
+buttonClean.addEventListener('click',()=>{
+    const h3 = document.querySelector('.numeroSorteado') 
+    
+    h3.remove()           
+})
